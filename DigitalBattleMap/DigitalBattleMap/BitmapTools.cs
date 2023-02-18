e2c0c9fd38cc1879b9ab609f3df5e4d2f7cfde59
MrBama <markvandenhurk@gmail.com>
2023-02-18 23:18:05 +0100

    Added drawing canvas

--- DigitalBattleMap/DigitalBattleMap/BitmapTools.cs
+++ DigitalBattleMap/DigitalBattleMap/BitmapTools.cs
@@ -4,6 +4,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+using System.Windows.Controls;
+using System.Windows.Ink;
 
 namespace DigitalBattleMap
 {
@@ -19,7 +22,19 @@ namespace DigitalBattleMap
             return gridBitMap;
         }
 
-        public static void DrawGrid(Bitmap bitmap, int gridSize)
+        public static Bitmap CreateEmptyBitmap()
+        {
+            return new Bitmap(_width, _height);
+        }
+
+        public static Bitmap CreateMap(Bitmap grid, StrokeCollection strokes, int inkCanvasWidth, int inkCanvasHeight)
+        {
+            var bitmap = new Bitmap(grid);
+            DrawStrokes(bitmap, strokes, inkCanvasWidth, inkCanvasHeight);
+            return bitmap;
+        }
+
+        private static void DrawGrid(Bitmap bitmap, int gridSize)
         {
             var xModulo = _width % gridSize;
             var yModulo = _height % gridSize;
@@ -39,6 +54,72 @@ namespace DigitalBattleMap
                 for (int y = startY; y < _height; y += gridSize)
                 {
                     graphics.DrawLine(blackPen, 0, y, _width, y);
+                }
+            }
+        }
+
+        private static void DrawStrokes(Bitmap bitmap, StrokeCollection strokes, int canvasWidth, int canvasHeight)
+        {
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                for (int strokeIndex = 0; strokeIndex < strokes.Count; strokeIndex++)
+                {
+                    var points = new List<PointF>();
+                    var drawingAttributs = strokes[strokeIndex].DrawingAttributes;
+                    var mediaColor = drawingAttributs.Color;
+                    var brush = new SolidBrush(Color.FromArgb(mediaColor.A, mediaColor.R, mediaColor.G, mediaColor.B));
+                    var penSize = (int)drawingAttributs.Width; // We always use the same width and height
+
+                    for (int pointIndex = 0; pointIndex < strokes[strokeIndex].StylusPoints.Count; pointIndex++)
+                    {
+                        var point = new PointF();
+                        point.X = (float)strokes[strokeIndex].StylusPoints[pointIndex].X;
+                        point.Y = (float)strokes[strokeIndex].StylusPoints[pointIndex].Y;
+
+                        var resizedX = Map(point.X, 0, canvasWidth, 0, _width) - penSize;
+                        var resizedY = Map(point.Y, 0, canvasHeight, 0, _height) - penSize;
+                        points.Add(new PointF(resizedX, resizedY));
+                    }
+
+                    SmoothLine(points, penSize);
+
+                    foreach (var point in points)
+                    {
+                        var resizedPenSize = Map(penSize, 0, canvasWidth, 0, _width);
+                        graphics.FillEllipse(brush, point.X, point.Y, resizedPenSize, resizedPenSize);
+                    }
+                }
+            }
+        }
+
+        private static float Map(float input, float inMin, float inMax, float outMin, float outMax)
+        {
+            return (input - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+        }
+
+        private static void SmoothLine(List<PointF> points, int penSize)
+        {
+            bool smoothRequired = true;
+
+            while (smoothRequired)
+            {
+                smoothRequired = false;
+                for (int i = 0; i < points.Count - 1; i++)
+                {
+                    var coord1 = points[i];
+                    var coord2 = points[i + 1];
+
+                    var dist = Math.Sqrt(Math.Pow(coord1.X - coord2.X, 2) + Math.Pow(coord1.Y - coord2.Y, 2));
+
+                    double penSize1 = (double)penSize;
+                    penSize1 /= 1;
+
+                    if (dist > penSize1)
+                    {
+                        var newPoint = new PointF((coord1.X + coord2.X) / 2, (coord1.Y + coord2.Y) / 2);
+                        points.Insert(i + 1, newPoint);
+                        smoothRequired = true;
+                    }
                 }
             }
         }
--- DigitalBattleMap/DigitalBattleMap/MainWindowViewModel.cs
+++ DigitalBattleMap/DigitalBattleMap/MainWindowViewModel.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Data;
+using System.Windows.Controls;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
@@ -19,34 +13,52 @@ namespace DigitalBattleMap
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         private Bitmap _gridBitmap;
+        private Bitmap _inkCanvasBitmap;
+        private double _inkCanvasWidth;
+        private double _inkCanvasHeight;
         private int _selectedMonitor = 0;
+        private double _penSize = 5;
         private IWindowService _windowService = null;
         private MapWindowViewModel _mapWindow = null;
         private ICommand _gridSizeEnterCommand;
         private ICommand _showMapCommand;
         private ICommand _windowClosingCommand;
+        private ICommand _drawingColorChangedCommand;
+        private ICommand _inkCanvasSizeOnStartupCommand;
 
         public MainWindowViewModel()
         {
             _gridBitmap = BitmapTools.CreateGrid(GridSize);
+            _inkCanvasBitmap = BitmapTools.CreateEmptyBitmap();
 
             _gridSizeEnterCommand = new RelayCommand(p => GridSizeChanged());
             _showMapCommand = new RelayCommand(p => ShowMap());
             _windowClosingCommand = new RelayCommand(p => WindowClosing());
+            _drawingColorChangedCommand = new RelayCommand(p => DrawingColorChanged((string)p));
+            _inkCanvasSizeOnStartupCommand = new RelayCommand(p => InkCanvasSizeOnStartup((double)p));
 
-            for(int i = 0; i < ScreenWrapper.GetScreenCount(); i++)
+            for (int i = 0; i < ScreenWrapper.GetScreenCount(); i++)
             {
                 MonitorNumbers.Add(i + 1);
             }
-            _selectedMonitor = 1;
 
+            InkCanvasDrawingAttributes.Width = PenSize;
+            InkCanvasDrawingAttributes.Height = PenSize;
+            EraserShape = new RectangleStylusShape(PenSize, PenSize);
+
+            _selectedMonitor = 1;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public BitmapSource GridBitMapSource 
-        { 
+        public BitmapSource GridBitMapSource
+        {
             get => _gridBitmap.ToBitmapImage();
+        }
+
+        public BitmapSource InkCanvasBackgroundBitMapSource
+        {
+            get => _inkCanvasBitmap.ToBitmapImage();
         }
 
         public ObservableCollection<int> MonitorNumbers { get; private set; } = new ObservableCollection<int>();
@@ -57,19 +69,38 @@ namespace DigitalBattleMap
 
             set
             {
-                if(value != _selectedMonitor)
+                if (value != _selectedMonitor)
                 {
                     _selectedMonitor = value;
+                    MonitorNumberChanged();
                 }
-                MonitorNumberChanged();
             }
         }
 
-        public int GridSize { get; set; } = 80;
+        public double PenSize
+        {
+            get => _penSize;
+            set
+            {
+                if (value != _penSize)
+                {
+                    _penSize = value;
+                    PenSizeChanged();
+                }
+            }
+        }
+
+        public int GridSize { get; set; } = 65;
+        public DrawingAttributes InkCanvasDrawingAttributes { get; set; } = new DrawingAttributes();
+        public InkCanvasEditingMode EditingMode { get; set; } = InkCanvasEditingMode.Ink;
+        public StylusShape EraserShape { get; set; }
+        public StrokeCollection Strokes { get; set; } = new StrokeCollection();
 
         public ICommand GridSizeEnterCommand { get => _gridSizeEnterCommand; }
         public ICommand ShowMapCommand { get => _showMapCommand; }
         public ICommand WindowClosingCommand { get => _windowClosingCommand; }
+        public ICommand DrawingColorChangedCommand { get => _drawingColorChangedCommand; }
+        public ICommand InkCanvasSizeOnStartupCommand { get => _inkCanvasSizeOnStartupCommand; }
 
         public void SetWindowService(IWindowService windowService)
         {
@@ -87,6 +118,7 @@ namespace DigitalBattleMap
 
         private void GridSizeChanged()
         {
+            GridSize = Math.Max(GridSize, 1);
             _gridBitmap = BitmapTools.CreateGrid(GridSize);
             NotifyPropertyChange(nameof(GridBitMapSource));
         }
@@ -99,12 +131,59 @@ namespace DigitalBattleMap
 
         private void ShowMap()
         {
-            _mapWindow.MapBitmapSource = _gridBitmap.ToBitmapImage();
+            var map = BitmapTools.CreateMap(_gridBitmap, Strokes, (int)_inkCanvasWidth, (int)_inkCanvasHeight);
+            _mapWindow.MapBitmapSource = map.ToBitmapImage();
         }
 
         private void WindowClosing()
         {
             _windowService.CloseAllWindows();
+        }
+
+        private void PenSizeChanged()
+        {
+            _penSize = Math.Clamp(_penSize, 1, 1000);
+            InkCanvasDrawingAttributes.Width = _penSize;
+            InkCanvasDrawingAttributes.Height = _penSize;
+            EraserShape = new EllipseStylusShape(_penSize, _penSize);
+            
+            NotifyPropertyChange(nameof(EraserShape));
+        }
+
+        private void DrawingColorChanged(string color)
+        {
+            if (EditingMode == InkCanvasEditingMode.EraseByPoint)
+            {
+                EditingMode = InkCanvasEditingMode.Ink;
+                NotifyPropertyChange(nameof(EditingMode));
+            }
+
+            switch (color)
+            {
+                case "Black":
+                    InkCanvasDrawingAttributes.Color = System.Windows.Media.Color.FromRgb(0, 0, 0);
+                    return;
+                case "Red":
+                    InkCanvasDrawingAttributes.Color = System.Windows.Media.Color.FromRgb(255, 0, 0);
+                    return;
+                case "Green":
+                    InkCanvasDrawingAttributes.Color = System.Windows.Media.Color.FromRgb(0, 255, 0);
+                    return;
+                case "Blue":
+                    InkCanvasDrawingAttributes.Color = System.Windows.Media.Color.FromRgb(0, 0, 255);
+                    return;
+                case "Eraser":
+                    EditingMode = InkCanvasEditingMode.EraseByPoint;
+                    NotifyPropertyChange(nameof(EditingMode));
+                    return;
+            }
+        }
+
+        private void InkCanvasSizeOnStartup(double width)
+        {
+            // It's enought to only use the width, since everything is done in a 16:9 ratio
+            _inkCanvasWidth = width;
+            _inkCanvasHeight = width / 16 * 9;
         }
     }
 }
